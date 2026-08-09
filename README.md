60d3f438cd1efa0756ca7b47643e7b3b003b0103
gladysfernandespr-commits <gladys.fernandespr@gmail.com>
2026-08-09 16:35:28 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# sprint-5-ferramentas
+Desenvolvimento de um aplicativo web usando Streamlit e deploy no Render
---
+++ app.py
@@ -0,0 +1,5 @@
+
+import streamilit as st
+st.header('Meu Primeiro Aplicativo')
+st.write('Ola Gladys! Obenvida ao meu primeiro aplicativo usando Streamlit!')
+
